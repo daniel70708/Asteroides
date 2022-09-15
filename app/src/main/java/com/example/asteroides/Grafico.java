47052af63973f21ccdb155987b494c1d4952055b
Angel Daniel <daniel_70708@hotmail.com>
2022-09-15 18:52:29 -0500

    Documentacion de la clase grafico

--- app/src/main/java/com/example/asteroides/Grafico.java
+++ app/src/main/java/com/example/asteroides/Grafico.java
@@ -7,37 +7,48 @@ import android.view.View;
 public class Grafico {
     private Drawable imagen; //Imagen que vamos a mostrar en el juego
     private int cordenadaXcentro,cordenadaYcentro; //Cordenadas (x,y) del centro de nuestra imagen
-    private int alto,ancho; //Alto y ancho de la imagen
-    private double cordenadaXincremento,cordenadaYincremento;//Posicion (x,y) del incremento de velocidad
+    private int alto,ancho; //Alto y ancho de la imagen (drawable)
+    private double cordenadaXincremento,cordenadaYincremento;// Modifica la posicion (x,y) según la velocidad de translación
     private double angulo, rotacion; //Angulo y rotación de nuestra imagen en el juego
     private int radioColicion; //Radio que nos indica cuando un objeto se cruza con otro y esto generará una colicion
-    private int cordenadaXanterior,cordenadaYanterior; //Cordenanda (x,y) anterior de nuestro gráfico
-    private int radioInval;
+    private int cordenadaXanterior,cordenadaYanterior; //Cordenanda (x,y) anterior de nuestro gráfico, esta cordenada se usa cuando la imagen se mueve
+    private int radioInval; //Incrementamos el radio de la imagen, cuando esta fue rotada
     private View view;
 
+    /** Inicializamos llamando a grafico con la vista donde se va a mostrar la imagen asi como el nombre de la drawable,
+     *  seguido sacamos el ancho y alto del drawable, calculamos el radio de colision y por último el radio inval que
+     *  representa un radio extendido ya que la imagen rotará por lo que abarcará más espacio
+     * */
     public Grafico(View view, Drawable imagen){
         this.view = view;
         this.imagen = imagen;
         ancho = imagen.getIntrinsicWidth(); //obtenemos en ancho de la imagen
-        alto = imagen.getIntrinsicHeight();//Obtenemos el alto de la imagen
-        radioColicion = (alto+ancho)/2; //Calculamos en radio de colicion
+        alto = imagen.getIntrinsicHeight();//obtenemos el alto de la imagen
+        radioColicion = (alto+ancho)/2; //calculamos en radio de colicion
         radioInval = (int) Math.hypot(ancho/2,alto/2);
     }
 
+    /** Dibujamos el drawable en la posición (x,y) y gurdamos los cambios para posteriormente rotarla de acuerdo
+     * al ángulo que le asignamos. Una vez dibujada la imagen (drawable), le decimos al canvas que hay que redibujar solo el
+     * rectangulo que hemos modificado y le pasamos las medidas del rectangulo. El drawable se mueve y por eso guardamos las
+     * cordenas (x,y) anteriores*/
     public void dibujarGrafico(Canvas canvas){
         int x = cordenadaXcentro - ancho/2;
         int y = cordenadaYcentro - alto/2;
-        imagen.setBounds(x,y,x+ancho, y+alto);//se va a dibujar la imagen en izquierda,ariba,derecha,abajo
-        canvas.save(); //Guardamos los cambios de la imagen
-        canvas.rotate((float)angulo, cordenadaXcentro,cordenadaYcentro); //Rotamos la imagen con un  angulo tomando las cordenas (x,y) como punto de anclaje
-        imagen.draw(canvas); //Dibujamos nuestra imagen con una posicion (x,y) y un angulo de rotación
+        imagen.setBounds(x,y,x+ancho, y+alto);// indicamos el rectangulo en donde se va a dibujar la imagen en izquierda,ariba,derecha,abajo
+        canvas.save(); //guardamos los cambios de la imagen
+        canvas.rotate((float)angulo, cordenadaXcentro,cordenadaYcentro); //rotamos la imagen con un  angulo tomando las cordenas (x,y) como punto de anclaje
+        imagen.draw(canvas); //dibujamos nuestra imagen con una posicion (x,y) y un angulo de rotación
         canvas.restore();
         view.invalidate(cordenadaXcentro-radioInval,cordenadaYcentro-radioInval,cordenadaYcentro+radioInval,cordenadaYcentro+radioInval);
         view.invalidate(cordenadaXanterior-radioInval,cordenadaYanterior-radioInval, cordenadaXanterior+radioInval,cordenadaYanterior+radioInval);
         cordenadaXanterior = cordenadaXcentro;
         cordenadaYanterior = cordenadaYcentro;
     }
-
+    /** Incrementa las posición (x,y) y el ángulo de nuestro drawable de acuerdo a un factor para posteriormete
+     * verificar si la cordenada de (x,y) es menor que cero lo que indicará que nuestro objeto se salio de la pantalla
+     * por lo que vamos a tomar el ancho y alto de nuestra vista y si la coordenada (x,y) es mayor que la vista la vamos a
+     * colocarla  en (0,0) */
     public void incrementaPosicion(double factor){
         cordenadaXcentro += cordenadaXincremento * factor;
         cordenadaYcentro += cordenadaYincremento * factor;
@@ -53,14 +64,15 @@ public class Grafico {
             cordenadaYcentro = 0;
         }
     }
-
+    /** */
     public double distancia(Grafico grafico){
         return Math.hypot(cordenadaXcentro - grafico.cordenadaXcentro, cordenadaYcentro - grafico.cordenadaYcentro);
     }
-
+    /** */
     public boolean verificarColision(Grafico grafico){
         return distancia(grafico) < (radioColicion + grafico.radioColicion);
     }
+     /*Geter and seter*/
 
     public Drawable getImagen() {
         return imagen;
