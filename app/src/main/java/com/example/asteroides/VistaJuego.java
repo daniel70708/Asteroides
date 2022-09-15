de416a2fe9490b87314c3ee997e7f3ae5f8d7229
Angel Daniel <daniel_70708@hotmail.com>
2022-09-15 12:51:06 -0500

    Se agrego la nave en vista juego, ademas de cambiar las imagenes de nave y asteroide

--- app/src/main/java/com/example/asteroides/VistaJuego.java
+++ app/src/main/java/com/example/asteroides/VistaJuego.java
@@ -15,16 +15,29 @@ public class VistaJuego extends View {
     private Vector<Grafico> asteroides;
     private int numeroAsteroides = 5;
     private int numeroFragmentos = 3;
+    //Nave
+    private Grafico nave;
+    private int giroNave;
+    private double aceleracionNave;
+    private static final int MAX_VELOCIDAD_NAVE = 20;
+    private static final int PASO_GIRO_NAVE = 5;
+    private static final float PASO_ACELERACION_NAVE = 0.5f;
+
 
     public VistaJuego(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
         Drawable drawableNave, drawableAsteroide,drawableMisil;
+
+        drawableNave = context.getResources().getDrawable(R.drawable.nave);
+        nave = new Grafico(this,drawableNave);
+
         drawableAsteroide = context.getResources().getDrawable(R.drawable.asteroide1);
         asteroides = new Vector<Grafico>();
         for (int i = 0; i<numeroAsteroides; i++){
             Grafico asteroide = new Grafico(this, drawableAsteroide);
             asteroide.setCordenadaYincremento(Math.random() * 4 -2);
             asteroide.setCordenadaXincremento(Math.random() * 4 - 2);
+
             asteroide.setAngulo((int) Math.random() * 360);
             asteroide.setRotacion((int) Math.random() * 8 - 4);
             asteroides.add(asteroide);
@@ -35,9 +48,14 @@ public class VistaJuego extends View {
     protected void onSizeChanged(int ancho, int alto, int ancho_anterior, int alto_anterior) {
         super.onSizeChanged(ancho, alto, ancho_anterior, alto_anterior);
         for (Grafico asteroide:asteroides){
-            asteroide.setCordenadaXcentro((int) Math.random() * ancho);
-            asteroide.setCordenadaYcentro((int) Math.random() * alto);
+            //do {
+                asteroide.setCordenadaXcentro((int) Math.random() * ancho);
+                asteroide.setCordenadaYcentro((int) Math.random() * alto);
+            //}while (asteroide.distancia(nave) < (ancho + alto) / 5);
+
         }
+        //nave.setCordenadaXcentro((int) 250);
+        //nave.setCordenadaYcentro((int) 400);
     }
 
     @Override
@@ -46,5 +64,6 @@ public class VistaJuego extends View {
         for (Grafico asteroide:asteroides){
             asteroide.dibujarGrafico(canvas);
         }
+        nave.dibujarGrafico(canvas);
     }
 }
