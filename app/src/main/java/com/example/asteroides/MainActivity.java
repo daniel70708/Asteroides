1d14b41d01ea5c354005cd52964bfecfc56839b9
Angel Daniel <daniel_70708@hotmail.com>
2022-09-15 11:23:27 -0500

    Se agrego la actividad de juego y se ejecuto la VistaJuego

--- app/src/main/java/com/example/asteroides/MainActivity.java
+++ app/src/main/java/com/example/asteroides/MainActivity.java
@@ -3,9 +3,13 @@ package com.example.asteroides;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.splashscreen.SplashScreen;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnJugar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +17,14 @@ public class MainActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnJugar = (Button) findViewById(R.id.botonIniciar);
+        btnJugar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent cambiarVentana = new Intent(MainActivity.this, Juego.class);
+                startActivity(cambiarVentana);
+            }
+        });
     }
 }
