7eaf4be18726927a301d5af2060ac3fc918ddf18
Angel Daniel <daniel_70708@hotmail.com>
2022-10-12 11:05:44 -0500

    Se agrego la clase Posicionamiento para calcular posicionamiento y radios del joystisck ademas del drawable disparo

---
+++ app/src/main/java/com/example/asteroides/Posicionamiento.java
@@ -0,0 +1,54 @@
+package com.example.asteroides;
+
+import android.view.View;
+
+public class Posicionamiento {
+
+    private int altoPantalla, anchoPantalla;
+    private int radioExterior, radioInterior;
+    private int posicionXjoystick, posicionYjoystick;
+    private int posicionXdisparo, posicionYdisparo;
+    private View view;
+
+    public Posicionamiento(View view) {
+        this.view = view;
+
+        altoPantalla = view.getResources().getDisplayMetrics().heightPixels;
+        anchoPantalla = view.getResources().getDisplayMetrics().widthPixels;
+
+        radioExterior = anchoPantalla / 12;
+        radioInterior = (radioExterior / 2) + 1;
+
+        posicionXjoystick = (anchoPantalla / 10) * 2;
+        int x = (altoPantalla / 10) * 3;
+        posicionYjoystick = altoPantalla - x;
+
+       /*/ posicionXdisparo = anchoPantalla - posicionXjoystick;
+        posicionYdisparo = posicionYjoystick;*/
+
+    }
+
+    public int getRadioExterior() {
+        return radioExterior;
+    }
+
+    public int getRadioInterior() {
+        return radioInterior;
+    }
+
+    public int getPosicionXjoystick() {
+        return posicionXjoystick;
+    }
+
+    public int getPosicionYjoystick() {
+        return posicionYjoystick;
+    }
+
+    public int getPosicionXdisparo() {
+        return posicionXdisparo;
+    }
+
+    public int getPosicionYdisparo() {
+        return posicionYdisparo;
+    }
+}
