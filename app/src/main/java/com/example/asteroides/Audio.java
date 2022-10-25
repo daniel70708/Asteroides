314b9326dd43be0d59f6545edceffcf2fff253a0
Angel Daniel <daniel_70708@hotmail.com>
2022-10-25 11:45:12 -0500

    Documentacion de la clase Audio y el ciclo de vida de la actividad de la activity Juego

--- app/src/main/java/com/example/asteroides/Audio.java
+++ app/src/main/java/com/example/asteroides/Audio.java
@@ -2,58 +2,48 @@ package com.example.asteroides;
 
 import android.content.Context;
 import android.media.AudioManager;
-import android.media.MediaPlayer;
 import android.media.SoundPool;
 
 public class Audio {
     Context context;
     SoundPool soundPool;
-    int ciclo,disparo,explosionAsteroideGrande,explosionAsteroidePequeno,movimiento;
+    int audioFondo,disparo,explosionAsteroideGrande,explosionAsteroideSmall;
 
+    /** Constructor en el cual recibimos el context y mandamos a llamar  a la clase soundpool
+     * definiendo los audios que usamos en la clase VistaJuego que son el ciclo de juego, disparo,
+     * explosion del asteroide grande como pequeño*/
     public Audio(Context context) {
         this.context = context;
         iniciarAudios();
     }
 
+    /** Iniciamos el soundpool con el cual vamos a reproducir un máximo de 10 audios en simultaneo y cargamos
+     * los audios de fondo,disparo,explosion de asteroide (grande y pequeño), sin ninguna prioridad para los
+     * audios*/
     public void iniciarAudios(){
-        soundPool = new SoundPool(7, AudioManager.STREAM_MUSIC, 0);
-        ciclo = soundPool.load(context,R.raw.ciclo,1);
+        soundPool = new SoundPool(10,AudioManager.STREAM_MUSIC, 0);
+        audioFondo = soundPool.load(context,R.raw.ciclo,0);
         disparo = soundPool.load(context, R.raw.disparo,0);
         explosionAsteroideGrande = soundPool.load(context,R.raw.explosion1,0);
-        explosionAsteroidePequeno = soundPool.load(context,R.raw.explosion2,0);
-        movimiento = soundPool.load(context,R.raw.movimiento,0);
+        explosionAsteroideSmall = soundPool.load(context,R.raw.explosion2,0);
     }
 
-    public void reproducirCiclo(){
-        soundPool.play(ciclo,1,1,1,-1,1);
+    /** Reproducimos el Audio de fondo con un ciclo de reprodución infinito*/
+    public void reproducirAudioFondo(){
+        soundPool.play(audioFondo,1,1,0,-1,1);
     }
 
-    public void pausarCiclo(){
-        soundPool.pause(ciclo);
-    }
-
-    public void reanudarCiclo(){
-        soundPool.resume(ciclo);
-    }
-
+    /** Reproducimos los audios de disparo, explosion asteroide (grande y pequeño) solo una vez, cuando
+     * el juego lo necesite*/
     public void reproducirDisparo(){
-        soundPool.play(disparo,1,1,0,0,1);
+        soundPool.play(disparo,1,1,1,0,1);
     }
 
     public void reproducirExplosionAsteroideGrande(){
-        soundPool.play(explosionAsteroideGrande,1,1,0,0,1);
+        soundPool.play(explosionAsteroideGrande,1,1,1,0,1);
     }
 
     public void reproducirExplosionAsteroidePequeno(){
-        soundPool.play(explosionAsteroidePequeno,1,1,0,0,1);
+        soundPool.play(explosionAsteroideSmall,1,1,1,0,1);
     }
-
-    public void reproducirMovimiento(){
-        soundPool.play(movimiento,1,1,0,0,1);
-    }
-
-    public void pausarMovimiento(){
-        soundPool.pause(movimiento);
-    }
-
 }
--- app/src/main/java/com/example/asteroides/Juego.java
+++ app/src/main/java/com/example/asteroides/Juego.java
@@ -1,14 +1,79 @@
 package com.example.asteroides;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Juego extends AppCompatActivity {
+    private VistaJuego vistaJuego;
+    public TextView textViewPuntuacion;
+    private ImageView imageViewJuego1, imageViewJuego2, imageViewJuego3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_juego);
+         setContentView(R.layout.activity_juego);
+
+         vistaJuego =(VistaJuego) findViewById(R.id.vistaJuego);
+         textViewPuntuacion = (TextView)findViewById(R.id.puntuacion);
+         imageViewJuego1 = (ImageView)findViewById(R.id.logoJuego1);
+         imageViewJuego2 = (ImageView)findViewById(R.id.logoJuego2);
+         imageViewJuego3 = (ImageView)findViewById(R.id.logoJuego3);
     }
+
+    /**Cuando la actividad va hacer pausada detenemos todos los audios que se esten reproduciendo y pausamos el
+     * hilo de ejecución de la clase VistaJuego*/
+    @Override
+    protected void onPause() {
+        super.onPause();
+        vistaJuego.audio.soundPool.autoPause();
+        vistaJuego.getHiloJuego().pausarHilo();
+    }
+
+    /**Cuando reanudamos la actividad, reproducimos los audios que quedaron en pausa y reanudamos el hilo
+     * de ejecución de la clase VistaJuego*/
+    @Override
+    protected void onResume() {
+        super.onResume();
+      /* try {
+            vistaJuego.getHiloJuego().sleep(1000);
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }*/
+        vistaJuego.audio.soundPool.autoResume();
+        vistaJuego.getHiloJuego().reanudarHilo();
+    }
+
+    /**Cuando la actividad va a ser destruida, pausamos todos los audios en reproducción, destruimos el soundoPool que
+     * controla el audio, admeás de detener el hilo de ejecución de la clase VistaJuego*/
+    @Override
+    protected void onDestroy() {
+        vistaJuego.audio.soundPool.autoPause();
+        vistaJuego.audio.soundPool.release();
+        vistaJuego.getHiloJuego().detenerHilo();
+        super.onDestroy();
+    }
+
+/*
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle estadoGuardado) {
+        super.onSaveInstanceState(estadoGuardado);
+        int puntaje = vistaJuego.getPuntaje();
+        int numeroJuegos = vistaJuego.getNumeroJuegos();
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle estadoGuardado) {
+        super.onRestoreInstanceState(estadoGuardado);
+         puntaje = estadoGuardado.getInt("puntaje");
+         numeroJuegos = estadoGuardado.getInt("numeroJuegos");
+    }*/
 }
