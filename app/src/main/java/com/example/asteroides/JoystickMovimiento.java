316b75e919bccc85939375a7815ef2748685d966
Angel Daniel <daniel_70708@hotmail.com>
2022-10-25 13:36:08 -0500

    Documentacion de ambas clases joystick para el movimiento e giro de la nave

--- app/src/main/java/com/example/asteroides/JoystickMovimiento.java
+++ app/src/main/java/com/example/asteroides/JoystickMovimiento.java
@@ -3,27 +3,32 @@ package com.example.asteroides;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.util.AndroidException;
 
-public class JoystickGiro {
-    private int radioCirculoExteriorGiro,cordenadaXcirculoExtGiro, cordenadaYcirculoExtGiro;
-    private int radioCirculoInteriorGiro,cordenadaXcirculoIntGiro,cordenadaYcirculoIntGiro;
+import androidx.core.content.ContextCompat;
+
+class JoystickMovimiento {
+
+    private int radioCirculoExterior,cordenadaXcirculoExterior, cordenadaYcirculoExterior;
+    private int radioCirculoInterior, cordenadaXcirculoInterior, cordenadaYcirculoInterior;
     private Paint circuloExteriorPaint, circuloInteriorPaint;
     private double distanciaJoystick;
     private boolean presionado;
     private double actuadorX,actuadorY;
 
-    /**Constructor de la clase con la que recibimos como parámetros la cordenada (x,y) de posición que tendŕa el joysctick,
-     * tanto para el círculo exterior como el interior, además de el radio de cada círculo*/
-    public JoystickGiro(int cordenadaX, int cordenadaY, int radioCiculoExterior, int radioCirculoInterior) {
-            //Asignamos la cordenada (x,y) a ambos circulos del joystick
-            cordenadaXcirculoExtGiro = cordenadaX;
-            cordenadaYcirculoExtGiro = cordenadaY;
-            cordenadaXcirculoIntGiro = cordenadaX;
-            cordenadaYcirculoIntGiro = cordenadaY;
+    /**Constructor de la clase con la que recibimos como parámetros la cordenada (x,y) de posición que tendŕa el
+     * joysctick, tanto para el círculo exterior como el interior, además de el radio de cada círculo*/
+    public JoystickMovimiento(int cordenadaX, int cordenadaY, int radioCirculoExt, int radioCirculoInt){
+        //Asignamos la cordenada (x,y) a ambos circulos del joystick
+        cordenadaXcirculoExterior = cordenadaX;
+        cordenadaYcirculoExterior = cordenadaY;
+        cordenadaXcirculoInterior = cordenadaX;
+        cordenadaYcirculoInterior = cordenadaY;
 
-            //Asignamos el radio a cada circulo
-            radioCirculoExteriorGiro = radioCiculoExterior;
-            radioCirculoInteriorGiro = radioCirculoInterior;
+        //Asignamos el radio a cada círculo
+        radioCirculoExterior = radioCirculoExt;
+        radioCirculoInterior = radioCirculoInt;
+
 
         //Definimos las características que van a tener el circulo exterior e interior
         int blancoPocaTrasparencia = Color.argb(200,255,255,255); //Creamos un color blanco con poca trasparencia para el circulo exterior
@@ -37,13 +42,14 @@ public class JoystickGiro {
         circuloInteriorPaint = new Paint();
         circuloInteriorPaint.setColor(blancoTransparente);
         circuloInteriorPaint.setStyle(Paint.Style.FILL_AND_STROKE); //Dibujamos el contorno y el relleno del circulo
+
     }
 
     /**Método con el que vamos a dibujar ambos circulos que componen el joystick*/
     public void draw(Canvas canvas) {
         //Para dibujar el circulo le pasamos como parámetros posición (x,y), el radio del circulo y las caráteristicas del Paint
-        canvas.drawCircle(cordenadaXcirculoExtGiro, cordenadaYcirculoExtGiro, radioCirculoExteriorGiro, circuloExteriorPaint);
-        canvas.drawCircle(cordenadaXcirculoIntGiro, cordenadaYcirculoIntGiro, radioCirculoInteriorGiro, circuloInteriorPaint);
+        canvas.drawCircle(cordenadaXcirculoExterior, cordenadaYcirculoExterior, radioCirculoExterior, circuloExteriorPaint);
+        canvas.drawCircle(cordenadaXcirculoInterior, cordenadaYcirculoInterior, radioCirculoInterior, circuloInteriorPaint);
     }
 
     /** Método que llamamos de manera recurrente para actualizar la posición (x,y) del círculo interior del joystick*/
@@ -55,20 +61,20 @@ public class JoystickGiro {
      * que indican hacia donde el usario esta realizando el movimiento*/
     public void actualizarPosicionCirculoInterior(){
         //A la cordenada inicial le sumamos la multiplicación del actuador por el radio del círculo exterior
-        cordenadaXcirculoIntGiro = (int) (cordenadaXcirculoExtGiro + (actuadorX * radioCirculoExteriorGiro));
-        cordenadaYcirculoIntGiro = (int) (cordenadaYcirculoExtGiro + (actuadorY * radioCirculoExteriorGiro));
+        cordenadaXcirculoInterior = (int) (cordenadaXcirculoExterior + (actuadorX * radioCirculoExterior));
+        cordenadaYcirculoInterior = (int) (cordenadaYcirculoExterior + (actuadorY * radioCirculoExterior));
     }
 
     /** Método que nos regresa un true si el usuario esta tocando la pantalla dentro del circulo exterior de nuestro
      * joystick y un false si el toque a la pantalla es fuera del joystick. Este cálculo se realiza restando la distancia
      * entre el toque del usuario y la posición del joystick (usando el teorema de Pitágoras)*/
     public boolean esPresionado(double posicionXusuario, double posicionYusuario) {
-        double catetoA = (cordenadaXcirculoExtGiro - posicionXusuario); //Restamos la cordenada X del joystick al toque X de la pantalla
-        double catetoB = (cordenadaYcirculoExtGiro - posicionYusuario); //Restamos la cordenada Y del joystick al toque Y de la pantalla
+        double catetoA = (cordenadaXcirculoExterior - posicionXusuario); //Restamos la cordenada X del joystick al toque X de la pantalla
+        double catetoB = (cordenadaYcirculoExterior - posicionYusuario); //Restamos la cordenada Y del joystick al toque Y de la pantalla
         distanciaJoystick = Math.hypot(catetoA, catetoB); //Calculamos la distancia con el teorema de Pitágoras
 
         //Si la distancia es menor que el radio del circulo del joystick, entonces sabemos que el toque es dentro del joystick
-        return distanciaJoystick < radioCirculoExteriorGiro;
+        return distanciaJoystick < radioCirculoExterior;
     }
 
     /** Cambiamos el valor de boleano presionado indicando que el usuario esta sobre el joystick o no*/
@@ -76,29 +82,29 @@ public class JoystickGiro {
         this.presionado = presionado;
     }
 
-    /**Obtenemos el valor de presionado*/
+     /**Obtenemos el valor de presionado*/
     public boolean getPresionado() {
         return presionado;
     }
 
     /**Función que calcula actuador x y actuador y, recibimos la posición (x,y) del toque del usuario y calculamos la distancia (usnado
      * el teorema de Pitágoras) entre la posición que recibimos y la posición del joystick. Si la distancia resultante es mayor al radio
-     * del joystick (círculo exterior) significa que el actuador indicará que el giro de la nave seŕa a máxima velocidad y si
-     * por el contrario es menor al radio del círculo exterior significa que el giro será moderado*/
+     * del joystick (círculo exterior) significa que el actuador indicará que el movimiento de la nave seŕa a máxima velocidad y si
+     * por el contrario es menor al radio del círculo exterior significa que el movimiento es moderado*/
     public void setActuador(double posicionXusuario, double posicionYusuario) {
         //Restamos la cordenada X del toque del usuario al cordenada X del círculo exterior
-        double deltaX = (posicionXusuario - cordenadaXcirculoExtGiro);
+        double deltaX = (posicionXusuario - cordenadaXcirculoExterior);
         //Restamos la cordenada Y del toque del usuario al cordenada Y del círculo exterior
-        double deltaY = (posicionYusuario - cordenadaYcirculoExtGiro);
+        double deltaY = (posicionYusuario - cordenadaYcirculoExterior);
         //Calculamos la distancia entre ambas posiciones
         double distanciaDelta = Math.hypot(deltaX,deltaY);
-        //Si la distanciaDelta es menor al radio del circulo exterior significa que el giro es moderado
-        if (distanciaDelta < radioCirculoExteriorGiro){
+        //Si la distancia es menor al radio del circulo exterior significa que el movimiento es moderado
+        if (distanciaDelta < radioCirculoExterior){
             //El valor del actuador x será deltaX entre radio del círculo exterior
-            actuadorX = deltaX / radioCirculoExteriorGiro;
+            actuadorX = deltaX / radioCirculoExterior;
             //El valor del actuador y será deltaY entre radio del círculo exterior
-            actuadorY = deltaY / radioCirculoExteriorGiro;
-        }else { //Significa que el moviento del joystick esta fuera del circulo del joystick por lo que el giro es a máxima velocidad
+            actuadorY = deltaY / radioCirculoExterior;
+        }else { //Significa que el moviento del joystick esta fuera del circulo del joystick por lo que el movimiento es a máxima velocidad
             //El valor del actuador x será deltaX entre la distancia de ambas x
             actuadorX = deltaX / distanciaDelta;
             //El valor del actuador y será deltaY entre la distancia de ambas y
@@ -113,11 +119,10 @@ public class JoystickGiro {
     }
 
     /**Obtenemos el actuador de X como de Y*/
-    public double getActuadorX() {
+    public double getActuadorX(){
         return actuadorX;
     }
-
-    public double getActuadorY() {
+    public double getActuadorY(){
         return actuadorY;
     }
 }
