9edd49ea1680feabc2619fc54aad3dbb72f5b005
Angel Daniel <daniel_70708@hotmail.com>
2022-11-22 13:04:10 -0600

    Se agrego el logo del splash y el de la aplicacion. (se eliminaron los logos antiguos)

--- app/src/main/java/com/example/asteroides/MainActivity.java
+++ app/src/main/java/com/example/asteroides/MainActivity.java
@@ -9,7 +9,7 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    Button btnJugar;
+    Button btnJugar, btnSalir, btnConfigurar, btnAcercaDe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,11 +19,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btnJugar = (Button) findViewById(R.id.botonIniciar);
+        btnAcercaDe = (Button) findViewById(R.id.botonAcercaDe);
+        btnSalir = (Button) findViewById(R.id.botonSalir);
+
         btnJugar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent cambiarVentana = new Intent(MainActivity.this, Juego.class);
                 startActivity(cambiarVentana);
+            }
+        });
+
+        btnAcercaDe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent mostrarAcercaDe = new Intent(MainActivity.this, AcercaDe.class);
+                startActivity(mostrarAcercaDe);
+            }
+        });
+
+        btnSalir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
             }
         });
     }
