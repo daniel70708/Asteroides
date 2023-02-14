066b1a0d5aaff3beafa120ed1f956656bac0346c
Angel Daniel <daniel_70708@hotmail.com>
2023-02-14 12:57:19 -0600

    Se creo el fragment que mostrara la lista de puntuaciones de los mejores jugadores

---
+++ app/src/main/java/com/example/asteroides/PuntuacionesFragment.java
@@ -0,0 +1,39 @@
+package com.example.asteroides;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class PuntuacionesFragment extends Fragment {
+    Button btnJugar, btnSalir, btnConfigurar, btnAcercaDe;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_puntuaciones, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+}
