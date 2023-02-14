305f95f4d8bacc0250f3b282db5b43b68d16df1e
Angel Daniel <daniel_70708@hotmail.com>
2023-02-14 13:24:04 -0600

    Se creo la clase puntuacion que almacenara los puntajes de los jugadores en una lista y mas adelante nos servira como entidad implementando room

---
+++ app/src/main/java/com/example/asteroides/Puntuacion.java
@@ -0,0 +1,13 @@
+package com.example.asteroides;
+
+import java.util.Date;
+
+public class Puntuacion {
+
+    public int ID_puntuacion;
+    public String nombre;
+    public int puntaje;
+    public Date fecha;
+    public int imagen;
+
+}
