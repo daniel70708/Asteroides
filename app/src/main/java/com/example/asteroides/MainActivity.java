d77ec7048c0ab886e0e68fff52642ab1af5c025a
Angel Daniel <daniel_70708@hotmail.com>
2023-02-16 21:34:09 -0600

    Se agrego el viewBinding para el acceso a las vistas, y se modifico el activity main para que solo sea el contendor que intercambia los fragments de acuerdo a la funcion que sea necesaria

--- app/src/main/java/com/example/asteroides/MainActivity.java
+++ app/src/main/java/com/example/asteroides/MainActivity.java
@@ -2,47 +2,37 @@ package com.example.asteroides;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.splashscreen.SplashScreen;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.asteroides.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
-    Button btnJugar, btnSalir, btnConfigurar, btnAcercaDe;
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //Mostramos el splash al iniciar la aplicación
         SplashScreen splashScreen = SplashScreen.installSplashScreen(this);
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
 
-        btnJugar = (Button) findViewById(R.id.botonIniciar);
-        btnAcercaDe = (Button) findViewById(R.id.botonAcercaDe);
-        btnSalir = (Button) findViewById(R.id.botonSalir);
 
-        btnJugar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent cambiarVentana = new Intent(MainActivity.this, Juego.class);
-                startActivity(cambiarVentana);
-            }
-        });
+        iniciarMenuPrincipal();
+    }
 
-        btnAcercaDe.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent mostrarAcercaDe = new Intent(MainActivity.this, AcercaDe.class);
-                startActivity(mostrarAcercaDe);
-            }
-        });
-
-        btnSalir.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                finish();
-            }
-        });
+    public void iniciarMenuPrincipal(){
+        Fragment newFragment = new MenuPrincipalFragment();
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.contenedorFragment, newFragment);
+        transaction.commit();
     }
 }
