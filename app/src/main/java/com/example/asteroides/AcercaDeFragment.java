6512fe6bc627cb26c167e8ad380a6a81b1a2c4cb
Angel Daniel <daniel_70708@hotmail.com>
2023-02-28 11:58:30 -0600

    Se agrego un fragment para el acerca de, ya que un dialogo es poco espacio para informacion necesaria

---
+++ app/src/main/java/com/example/asteroides/AcercaDeFragment.java
@@ -0,0 +1,35 @@
+package com.example.asteroides;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.asteroides.databinding.FragmentAcercaDeBinding;
+
+public class AcercaDeFragment extends Fragment {
+    private FragmentAcercaDeBinding binding;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        binding = FragmentAcercaDeBinding.inflate(inflater, container, false);
+        View view = binding.getRoot();
+        return view;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
